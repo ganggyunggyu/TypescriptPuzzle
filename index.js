3c89dc0910c1e878194c1126bb99a7cac20de5fc
강경규 <qwzx16@naver.com>
2023-07-03 16:17:22 +0900

    puzzlegame

---
+++ index.js
@@ -0,0 +1,91 @@
+const items = document.querySelectorAll('.item');
+const results = document.querySelectorAll('.result');
+const itemsImg = document.querySelectorAll('.item > img');
+const resultsImg = document.querySelectorAll('.result > img');
+const puzzle = document.querySelectorAll('.puzzle > img');
+const noImg = 'http://127.0.0.1:5500/img/No_img.jpg';
+let randomNumArr = [];
+let imgIndex = 0;
+let puzzleIndex = 0;
+let resultIndex = 0;
+let imgBox = '';
+let dropImgBox = '';
+let resultImgBox = '';
+const randomNumber = () => {
+    for (let i = 0; i < items.length; i++) {
+        let num = 0;
+        num = Math.ceil(Math.random() * items.length);
+        if (!randomNumArr.includes(num)) {
+            randomNumArr.push(num);
+        }
+        else {
+            i--;
+        }
+    }
+};
+randomNumber();
+const makeImg = () => {
+    randomNumArr.forEach((el, i) => {
+        if (itemsImg[i] instanceof HTMLImageElement) {
+            itemsImg[i].src = `/img/deer_${el}.jpg`;
+        }
+    });
+};
+makeImg();
+for (let i = 0; i < puzzle.length; i++) {
+    puzzle[i].addEventListener('dragstart', () => {
+    });
+    puzzle[i].addEventListener('dragover', () => {
+    });
+}
+// for(let i=0; i<items.length; i++){
+//         itemsImg[i].addEventListener('dragover', (e)=>{
+//             e.preventDefault()
+//         })
+//         itemsImg[i].addEventListener('dragstart', (e)=>{
+//             imgBox = itemsImg[i].src
+//             puzzleIndex = i
+//             console.log('puzzle start ' , puzzleIndex, i)
+//         })
+//         itemsImg[i].addEventListener('drop', (e)=>{
+//             e.preventDefault();
+//             console.log('puzzle drop' )
+//             dropImgBox = imgBox
+//             resultsImg[resultIndex].src = noImg
+//             itemsImg[puzzleIndex].src = itemsImg[i].src
+//             itemsImg[i].src = imgBox
+//             if(itemsImg[i].src == noImg){
+//                 itemsImg[i].draggable = false;
+//             }else{
+//                 itemsImg[i].draggable = true;
+//             }
+//         })
+//     }
+// for(let i=0; i<results.length; i++){
+//         results[i].addEventListener('dragover', (e)=>{
+//             e.preventDefault()
+//         })
+//         results[i].addEventListener('dragstart', ()=>{
+//             imgBox = resultsImg[i].src
+//             resultIndex = i;
+//             console.log('result start , ' + resultIndex)
+//         })
+//         results[i].addEventListener('drop', (e)=>{
+//             console.log(i)
+//             e.preventDefault();
+//             console.log('result drop')
+//             itemsImg[puzzleIndex].src = noImg
+//             resultsImg[resultIndex].src = resultsImg[i].src;
+//             resultsImg[i].src = imgBox
+//             if(resultsImg[i].src == noImg){
+//                 resultsImg[i].draggable = false;
+//             }else{
+//                 resultsImg[i].draggable = true;
+//             }
+//             if(itemsImg[puzzleIndex].src == noImg){
+//                 itemsImg[puzzleIndex].draggable = false;
+//             }else{
+//                 itemsImg[puzzleIndex].draggable = true;
+//             }
+//         })
+//     }
